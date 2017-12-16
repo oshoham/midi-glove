4ee957b5cd71d3d44ba017a141301d276de80e1d
Oren Shoham <oren.avraham.shoham@gmail.com>
2017-12-16 14:31:17 -0500

    Add comments

--- midi_glove.ino
+++ midi_glove.ino
@@ -1,6 +1,47 @@
 /*
- * Based on Adafruit's Bluefruit LE nRF51 MIDI example:
+ * MIDI Glove (Right Hand)
+ * 
+ * Controller code for a Blueooth MIDI controller glove, made as a
+ * final project for Tom Igoe's Fall 2017 Introduction to Phyiscal
+ * Computing class at NYU ITP.
+ * 
+ * Written December 15, 2017
+ * By Oren Shoham
+ * 
+ * 
+ * Intended to run on an Adafruit Feather 32u4 Bluefruit LE
+ * https://www.adafruit.com/product/2829
+ * 
+ * Bluetooth MIDI code adapted from Adafruit's Bluefruit LE nRF51 MIDI example:
  * https://github.com/adafruit/Adafruit_BluefruitLE_nRF51/blob/bdad61148ee3a4a1a9c84bc76fcf962a60ca3b11/examples/midi/midi.ino
+ * 
+ * Before you can run this code on your Feather, you may need to
+ * upgrade its firmware to the latest version using Adafruit's
+ * Bluefruit LE Connect app.
+ * https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/software-resources
+ * 
+ * 
+ * To connect to an iOS device:
+ * 
+ * 1) Download midimittr (https://itunes.apple.com/us/app/midimittr/id925495245?mt=8)
+ * 2) Download a MIDI-compatible audio app, e.g.
+ *     - iGrand Piano Free (https://itunes.apple.com/us/app/igrand-piano-free-for-ipad/id562914032?mt=8)
+ *     - DrumsLive Lite (https://itunes.apple.com/us/app/drumslive-lite-touch-and-midi-drums/id600089094?mt=8)
+ * 3) Enable Bluetooth on your device
+ * 4) Turn on the glove and wait for it to enter pairing mode (the LEDs will start fading)
+ * 5) Open the midimittr app and under Clients select "MIDI Glove Right Hand"
+ * 6) Once you see the 'Connected' label, open your audio app
+ * 7) Tap the fingers of the glove and you should see notes playing on the app
+ *
+ * To connect to a computer running OS X:
+ *
+ * 1) Open the Audio MIDI Setup application on your computer
+ * 2) Enable Bluetooth on your computer
+ * 3) Turn on the glove and wait for it to enter pairing mode (the LEDs will start fading)
+ * 4) Click on the Bluetooth icon in Audio MIDI Setup
+ * 5) Find "MIDI Glove Right Hand" in the list of devices and click "Conncect"
+ * 6) Open a DAW like Ableton Live, Logic Pro, or GarageBand
+ * 7) Use the glove like any other MIDI instrument
  */ 
 
 #include <Arduino.h>
@@ -13,19 +54,6 @@
 
 #define FACTORYRESET_ENABLE         1
 #define MINIMUM_FIRMWARE_VERSION    "0.7.0"
-
-// This app was tested on iOS with the following apps:
-//
-// https://itunes.apple.com/us/app/midimittr/id925495245?mt=8
-// https://itunes.apple.com/us/app/igrand-piano-free-for-ipad/id562914032?mt=8
-//
-// To test:
-// - Run this sketch and open the Serial Monitor
-// - Open the iGrand Piano Free app
-// - Open the midimittr app on your phone and under Clients select "Adafruit Bluefruit LE"
-// - When you see the 'Connected' label switch to the Routing panel
-// - Set the Destination to 'iGrand Piano'
-// - Switch to the iGrand Piano Free app and you should see notes playing one by one
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 Adafruit_BLEMIDI midi(ble);
@@ -45,43 +73,41 @@ const int pinkyLEDPin = 11;
 const int inputPins[5] = {thumbInputPin, indexInputPin, middleInputPin, ringInputPin, pinkyInputPin};
 const int LEDPins[5] = {thumbLEDPin, indexLEDPin, middleLEDPin, ringLEDPin, pinkyLEDPin};
 
+const int tapDebounceThreshold = 100;
+
 bool isConnected = false;
 
 bool fingerStates[5] = {false, false, false, false, false};
-int thresholds[5] = {100, 200, 400, 500, 400};
+int thresholds[5] = {200, 200, 500, 500, 400};
 int notes[5] = {60, 62, 64, 65, 67};
+unsigned long lastTimePlayed[5] = {0, 0, 0, 0, 0};
 
-unsigned long initializedAt = 0;
+const int ledFadeRate = 30;
 unsigned long previousMillis = 0;
 int ledFadeValue = 0;
 bool fadingUp = true;
 
-// A small helper
+// error logging helper
 void error(const __FlashStringHelper*err) {
   Serial.println(err);
   while (1);
 }
 
-// callback
-void connected(void)
-{
+void connected(void) {
   isConnected = true;
-  Serial.println(F(" CONNECTED!"));
+  Serial.println(F("connected"));
   flashLEDs(4);
   delay(1000);
 }
 
-void disconnected(void)
-{
+void disconnected(void) {
   Serial.println("disconnected");
   isConnected = false;
   ledFadeValue = 0;
-  initializedAt = millis();
-  flashLEDs(3);
+  flashLEDs(4);
 }
 
-void BleMidiRX(uint16_t timestamp, uint8_t status, uint8_t byte1, uint8_t byte2)
-{
+void BleMidiRX(uint16_t timestamp, uint8_t status, uint8_t byte1, uint8_t byte2) {
   Serial.print("[MIDI ");
   Serial.print(timestamp);
   Serial.print(" ] ");
@@ -110,9 +136,21 @@ void flashLEDs(int times) {
   }
 }
 
-void setup(void)
-{
-  /* Flash some lights to show that the glove is on */
+void logSensorValues() {
+  Serial.print("thumb: ");
+  Serial.print(analogRead(thumbInputPin));
+  Serial.print(", index: ");
+  Serial.print(analogRead(indexInputPin));
+  Serial.print(", middle: ");
+  Serial.print(analogRead(middleInputPin));
+  Serial.print(", ring: ");
+  Serial.print(analogRead(ringInputPin));
+  Serial.print(", pinky: ");
+  Serial.println(analogRead(pinkyInputPin));
+}
+
+void setup(void) {
+  // flash the LEDs to show that the glove is on
   for (int i = 0; i < 5; i++) {
     pinMode(LEDPins[i], OUTPUT);
     analogWrite(LEDPins[i], 255);
@@ -129,7 +167,7 @@ void setup(void)
 
   Serial.begin(115200);
 
-  /* Initialise the module */
+  // initialise the module
   Serial.print(F("Initialising the Bluefruit LE module: "));
 
   if (!ble.begin(VERBOSE_MODE))
@@ -140,7 +178,7 @@ void setup(void)
 
   if (FACTORYRESET_ENABLE)
   {
-    /* Perform a factory reset to make sure everything is in a known state */
+    // perform a factory reset to make sure everything is in a known state
     Serial.println(F("Performing a factory reset: "));
     if (!ble.factoryReset()) {
       error(F("Couldn't factory reset"));
@@ -150,20 +188,20 @@ void setup(void)
   ble.echo(false);
 
   Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
+  // print Bluefruit information
   ble.info();
 
-  /* Change the device name to make it easier to find */
+  // change the device name to make it easier to find
   Serial.println(F("Setting device name to 'MIDI Glove Right Hand': "));
   if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=MIDI Glove Right Hand" )) ) {
     error(F("Could not set device name?"));
   }
 
-  /* Set BLE callbacks */
+  // set BLE callbacks
   ble.setConnectCallback(connected);
   ble.setDisconnectCallback(disconnected);
 
-  // Set MIDI RX callback
+  // set MIDI RX callback
   midi.setRxCallback(BleMidiRX);
 
   Serial.println(F("Enable MIDI: "));
@@ -174,18 +212,21 @@ void setup(void)
 
   ble.verbose(false);
   Serial.print(F("Waiting for a connection..."));
-  initializedAt = millis();
 }
 
 void loop(void) {
   // interval for each scanning ~ 500ms (non blocking)
   ble.update(500);
 
+  // uncomment the following line for debugging
+//  logSensorValues();
+
   unsigned long currentMillis = millis();
 
-  // bail if not connected
   if (!isConnected) {
-    if (currentMillis - previousMillis >= 50) {
+    // if we're not connected, fade the finger LEDs up and down
+    // to show that we're waiting to pair
+    if (currentMillis - previousMillis >= ledFadeRate) {
       previousMillis = currentMillis;
       for (int j = 0; j < 5; j++) {
         analogWrite(LEDPins[j], ledFadeValue);
@@ -203,27 +244,29 @@ void loop(void) {
         ledFadeValue -= 5;
       }
     }
+    // bail on the loop function because we aren't connected
     return;
   }
 
-  Serial.print("A0: ");
-  Serial.print(analogRead(A0));
-  Serial.print(", A1: ");
-  Serial.print(analogRead(A1));
-  Serial.print(", A2: ");
-  Serial.print(analogRead(A2));
-  Serial.print(", A3: ");
-  Serial.print(analogRead(A3));
-  Serial.print(", A4: ");
-  Serial.println(analogRead(A4));
-
+  // for each finger sensor
+  // if the sensor is over its threshold:
+  //   turn on the corresponding LED
+  //   and if the corresponding MIDI note wasn't already playing:
+  //     play it
+  // otherwise:
+  //   turn off the corresponding LED
+  //   and if the corresponding MIDI was playing:
+  //     stop playing it
   for (int i = 0; i < 5; i++) {
     int value = analogRead(inputPins[i]);
     int note = notes[i];
+    
     if (value > thresholds[i]) {
-      if (!fingerStates[i]) {
-        midi.send(0x90, notes[i], map(value, thresholds[i], 1023, 63, 127));
+      int velocity = map(value, thresholds[i], 1023, 63, 127);
+      if (!fingerStates[i] && currentMillis - lastTimePlayed[i] >= tapDebounceThreshold) {
+        midi.send(0x90, notes[i], velocity);
         fingerStates[i] = true;
+        lastTimePlayed[i] = currentMillis;
       }
       digitalWrite(LEDPins[i], HIGH);
     } else {
@@ -231,6 +274,7 @@ void loop(void) {
         midi.send(0x80, notes[i], 0x64);
         fingerStates[i] = false;
       }
+      
       digitalWrite(LEDPins[i], LOW);
     }
   }
